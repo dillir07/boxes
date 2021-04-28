b4e7a60c65274f572c872561435970c2cabc9c8c
Dilli Babu R <dillir07@gmail.com>
2021-04-28 12:24:26 +0530

    - added jsdoc comments - renamed Box to Tile - fixed array index issue - create array to hold current row items

--- Grid.ts
+++ Grid.ts
@@ -1,79 +1,112 @@
+/**
+   * Grid class represents a Grid like strucutre with rows & columns
+   * This is like a 4 ways linked lists, each tile has top, bottom, right & left
+   * links to another tile in those directions.
+   */
 class Grid {
   itemsCount: number;
   rowsCount: number;
   columnsCount: number;
-  head?: Box;
-  tail?: Box;
-  items: Box[];
-  // rows: Box[];
-  private rowStart?: Box;
+  head?: Tile;
+  tail?: Tile;
+  items: Tile[];
+  rows: Tile[][];
+  private rowStart?: Tile;
 
+  /**
+   * Initializes Grid class
+   * @param rowsCount - no of rows for the grid, also defines rowsCount property of the grid.
+   * @param columnsCount - no of rows for the grid, also defines rowsCount property of the grid.
+   * @param values - no of rows for the grid, also defines rowsCount property of the grid.
+   */
   constructor(rowsCount: number, columnsCount: number, values: number[]) {
     this.rowsCount = rowsCount;
     this.columnsCount = columnsCount;
     this.head = undefined;
     this.tail = undefined;
     this.itemsCount = values.length;
-    this.items = values.map((value) => new Box(value));
+    this.items = values.map((value) => new Tile(value));
+    this.rows = [];
 
     for (let rowCounter = 1; rowCounter <= this.rowsCount; rowCounter++) {
+      const currentRow: Tile[] = [];
       for (
         let columnCounter = 1;
         columnCounter <= this.columnsCount;
         columnCounter++
       ) {
-        let box = this.items[(rowCounter * columnCounter) - 1];
-        if (columnCounter === columnsCount) {
-          this.rowStart = undefined;
-        }
+        const tile = this
+          .items[
+            (((rowCounter - 1) * this.columnsCount) + columnCounter) - 1
+          ];
 
-        if (this.head === null) {
-          this.head = box;
+        if (this.head === undefined) {
+          this.head = tile;
         }
 
         if (columnCounter === 1) {
-          this.rowStart = box;
-        }
-
-        if (columnCounter === columnsCount) {
-          this.rowStart = box;
+          this.rowStart = tile;
         }
 
         if (this.rowStart) {
-          box.left = this.rowStart;
-          this.rowStart.right = box;
+          tile.left = this.rowStart;
+          this.rowStart.right = tile;
+          currentRow.push(tile);
+        }
+
+        if (columnCounter === columnsCount && this.rowStart) {
+          this.rows.push(currentRow);
+          this.rowStart = undefined; // no need for this, but just to be sure.
         }
 
         if (this.tail) {
-          box.left = this.tail;
-          this.tail.right = box;
+          tile.left = this.tail;
+          this.tail.right = tile;
         }
-        this.tail = box;
+        this.tail = tile;
       }
     }
   }
 
+  /**
+   * 
+   * @returns the columnsCount of the grid
+   */
   getWidth(): number {
     return this.columnsCount;
   }
 
+  /**
+   * 
+   * @returns rowsCount of the grid
+   */
   getHeight(): number {
     return this.rowsCount;
   }
 
+  /**
+   * 
+   * @returns itemsCount of the grid
+   */
   getCount(): number {
     return this.itemsCount;
   }
 
+  /**
+   * prints all the list of items in a grid structure.
+   */
   print(): void {
     for (let rowCounter = 1; rowCounter <= this.rowsCount; rowCounter++) {
-      let rowValues: string = "";
+      let rowValues: string;
+      rowValues = "";
       for (
         let columnCounter = 1;
         columnCounter <= this.columnsCount;
         columnCounter++
       ) {
-        rowValues += (this.items[(rowCounter * columnCounter) - 1].value) +
+        rowValues += (this
+          .items[(((rowCounter - 1) * this.columnsCount) + columnCounter) - 1]
+          .value) +
           "\t";
         if (columnCounter === this.columnsCount) {
           console.log(rowValues);
@@ -84,12 +117,15 @@ class Grid {
   }
 }
 
-class Box {
+/**
+ * 
+ */
+class Tile {
   value: number;
-  top?: Box;
-  bottom?: Box;
-  left?: Box;
-  right?: Box;
+  top?: Tile;
+  bottom?: Tile;
+  left?: Tile;
+  right?: Tile;
 
   constructor(value: number) {
     this.value = value;
@@ -100,8 +136,10 @@ class Box {
   }
 }
 
-let g = new Grid(1, 5, [1, 2, 3, 4, 5]);
+let g = new Grid(3, 5, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]);
 console.log("Count:", g.getCount());
 console.log("Height:", g.getHeight());
-console.log("Weight:", g.getWidth());
+console.log("Width:", g.getWidth());
 g.print();
+console.log(g.rows);
+console.log("nothing");
