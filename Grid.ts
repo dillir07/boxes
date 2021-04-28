b55b80d49573f732cb2cf0c02151ee019828060b
Dilli Babu R <dillir07@gmail.com>
2021-04-28 14:56:50 +0530

    - changed undefined to null where appropriate added linkItems method to link tiles vertically (top & bottom) added getById method added getByRowAndColumnIndex method

--- Grid.ts
+++ Grid.ts
@@ -1,4 +1,6 @@
 /**
+   * Grid
+   * ----
    * Grid class represents a Grid like strucutre with rows & columns
    * This is like a 4 ways linked lists, each tile has top, bottom, right & left
    * links to another tile in those directions.
@@ -7,8 +9,8 @@ class Grid {
   itemsCount: number;
   rowsCount: number;
   columnsCount: number;
-  head?: Tile;
-  tail?: Tile;
+  head: Tile | null;
+  tail: Tile | null;
   items: Tile[];
   rows: Tile[][];
   private rowStart?: Tile;
@@ -22,10 +24,10 @@ class Grid {
   constructor(rowsCount: number, columnsCount: number, values: number[]) {
     this.rowsCount = rowsCount;
     this.columnsCount = columnsCount;
-    this.head = undefined;
-    this.tail = undefined;
+    this.head = null;
+    this.tail = null;
     this.itemsCount = values.length;
-    this.items = values.map((value) => new Tile(value));
+    this.items = values.map((value, index) => new Tile(index, value));
     this.rows = [];
 
     for (let rowCounter = 1; rowCounter <= this.rowsCount; rowCounter++) {
@@ -40,7 +42,7 @@ class Grid {
             (((rowCounter - 1) * this.columnsCount) + columnCounter) - 1
           ];
 
-        if (this.head === undefined) {
+        if (this.head === null) {
           this.head = tile;
         }
 
@@ -49,7 +51,7 @@ class Grid {
         }
 
         if (this.rowStart) {
-          tile.left = this.rowStart;
+          // tile.left = this.rowStart;
           this.rowStart.right = tile;
           currentRow.push(tile);
         }
@@ -66,6 +68,63 @@ class Grid {
         this.tail = tile;
       }
     }
+    this.linkItems();
+  }
+
+  /**
+   * links items vertically, i.e., top & bottom properties of each tile will be
+   * filled appropriately.
+   * this is done by looping row and column wise and for each tile
+   * we identify top and bottom tiles by calculating it's position using
+   * current row and column counters and assign accordingly.
+   * top property for top row items and bottom property for bottom row items would be null.
+   */
+  linkItems(): void {
+    for (let rowCounter = 1; rowCounter <= this.rowsCount; rowCounter++) {
+      for (
+        let columnCounter = 1;
+        columnCounter <= this.columnsCount;
+        columnCounter++
+      ) {
+        const tile = this
+          .items[
+            (((rowCounter - 1) * this.columnsCount) + columnCounter) - 1
+          ];
+
+        if (rowCounter === 1) tile.top = null;
+        else {
+          if (rowCounter > 1) {
+            tile.top = this
+              .items[((rowCounter - 2) * this.columnsCount) + columnCounter];
+          }
+        }
+        if (rowCounter === this.rowsCount) tile.bottom = null;
+        else {
+          tile.bottom = this
+            .items[((rowCounter) * this.columnsCount) + columnCounter];
+        }
+      }
+    }
+  }
+
+  /**
+   * Returns value by index (0 based array index)
+   * @param id - index
+   * @returns - value
+   */
+  getById(id: number): number {
+    return this.items[id].value;
+  }
+
+  /**
+   * Returns value by taking row & column index
+   * both row and column indicies are 0 based.
+   * @param rowIndex 
+   * @param columnIndex 
+   * @returns 
+   */
+  getByRowAndColumnIndex(rowIndex: number, columnIndex: number): number {
+    return this.items[((rowIndex - 1) * columnIndex) - 1].value;
   }
 
   /**
@@ -118,28 +177,34 @@ class Grid {
 }
 
 /**
- * 
+ * Tile
+ * ----
+ * @class Grid is made up of tiles.
+ * Each tile will have a value and unique id (index)
+ * Also will contain top, bottom, left and right properties linking to other tiles
+ * in those respective directions.
  */
 class Tile {
+  id: number;
   value: number;
-  top?: Tile;
-  bottom?: Tile;
-  left?: Tile;
-  right?: Tile;
+  top: Tile | null;
+  bottom: Tile | null;
+  left: Tile | null;
+  right: Tile | null;
 
-  constructor(value: number) {
+  constructor(id: number, value: number) {
+    this.id = id;
     this.value = value;
-    this.top = undefined;
-    this.bottom = undefined;
-    this.left = undefined;
-    this.right = undefined;
+    this.top = null;
+    this.bottom = null;
+    this.left = null;
+    this.right = null;
   }
 }
 
-let g = new Grid(3, 5, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]);
+const g = new Grid(5, 3, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]);
 console.log("Count:", g.getCount());
 console.log("Height:", g.getHeight());
 console.log("Width:", g.getWidth());
 g.print();
-console.log(g.rows);
-console.log("nothing");
+console.log();
